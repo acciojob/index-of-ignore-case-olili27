9f6e50ac6a77f3e38bf032360067c164de502fba
olili27 <timmolili27@gmail.com>
2024-06-18 18:42:19 +0300

    finish assignment

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  let count = 0;
+  let index = 0;
+  let len = s2.length
+
+  while((index + len) <= s1.length) {
+   let newSubstring = s1.substring(index, index + len);
+
+   console.log("sub", newSubstring);
+
+   if(newSubstring == s2) {
+    return index
+   }
+   index++;
+
+  }
+  console.log("i am out");
+  return -1
 }
 
 // Please do not change the code below
